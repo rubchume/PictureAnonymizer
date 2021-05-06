950febb6427faed106b1443fa3a4ac915aa7f482
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-06 20:20:25 +0200

    Redirecting to home implemented

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
     path('upload_picture/', views.UploadPictureView.as_view(), name='upload_picture'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
 ]
