beacf16cb082603e0c64b275e2326d00e6d69cf7
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 17:44:07 +0200

    Espagueti

--- photography_anonymizator/urls.py
+++ photography_anonymizator/urls.py
@@ -19,8 +19,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("core.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path('admin/', admin.site.urls),
+        path('', include("core.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
