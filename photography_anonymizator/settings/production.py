d493a5e1fa73615f4f5f17177f4d45e0e97cfcad
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 13:20:36 +0200

    Fix typo

--- photography_anonymizator/settings/production.py
+++ photography_anonymizator/settings/production.py
@@ -1,5 +1,5 @@
 # In a production environment, the next environment variable must be set:
-# DJANGO_SETTINGS_MODULE=WhatsappAnalyzer.settings.production
+# DJANGO_SETTINGS_MODULE=photography_anonymizator.settings.production
 
 from .base import *
 
