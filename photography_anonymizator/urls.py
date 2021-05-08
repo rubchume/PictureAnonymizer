d1c6f70961b843a3cfd33cd32e323fc5b0d32f24
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 19:10:40 +0200

    Será esto?

--- photography_anonymizator/urls.py
+++ photography_anonymizator/urls.py
@@ -24,10 +24,7 @@ urlpatterns = [
     path('', include("core.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# else:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
