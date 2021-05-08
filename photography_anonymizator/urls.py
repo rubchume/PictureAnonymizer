22df3a63c7193ada3641da7e0f4b92ad87e09f61
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 17:54:38 +0200

    Urls.py correction

--- photography_anonymizator/urls.py
+++ photography_anonymizator/urls.py
@@ -19,11 +19,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-urlpatterns = (
-    [
+urlpatterns = [
         path('admin/', admin.site.urls),
         path('', include("core.urls")),
     ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
