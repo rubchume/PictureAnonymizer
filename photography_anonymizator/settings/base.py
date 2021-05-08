54ca1f80344c25e7968f09b10cc54a4935c16bea
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 13:36:56 +0200

    Update BASE_DIR in settings.base.py

--- photography_anonymizator/settings/base.py
+++ photography_anonymizator/settings/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "static_root/"
+STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
