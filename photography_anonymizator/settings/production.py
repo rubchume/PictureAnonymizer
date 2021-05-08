53972a1f2f31cbb573ef66442d2ff725edd75dea
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 16:50:48 +0200

    Change to use PostgreSQL database in Heroku

--- photography_anonymizator/settings/production.py
+++ photography_anonymizator/settings/production.py
@@ -1,12 +1,28 @@
 # In a production environment, the next environment variable must be set:
 # DJANGO_SETTINGS_MODULE=photography_anonymizator.settings.production
-
+import dj_database_url
 import django_heroku
 
 from .base import *
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'photography_anonymyzer',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
