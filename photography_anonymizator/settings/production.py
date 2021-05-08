ade01b01b1d7bf3102b6eb894e632f28b1bb87e5
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 13:19:30 +0200

    Create production settings.py

---
+++ photography_anonymizator/settings/production.py
@@ -0,0 +1,7 @@
+# In a production environment, the next environment variable must be set:
+# DJANGO_SETTINGS_MODULE=WhatsappAnalyzer.settings.production
+
+from .base import *
+
+
+ALLOWED_HOSTS = ['.herokuapp.com']
