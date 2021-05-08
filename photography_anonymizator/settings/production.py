e114d9795a2be0f9ad39bdbdc2c8ae960a0575e6
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 17:25:56 +0200

    Rearrange WhiteNoiseMiddleware

--- photography_anonymizator/settings/production.py
+++ photography_anonymizator/settings/production.py
@@ -14,7 +14,16 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
 
-MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 DATABASES = {
     'default': {
