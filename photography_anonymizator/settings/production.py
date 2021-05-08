8da2026b8f1cbe9ac62d6d4300e417940df80d04
Rubén Chuliá Mena <rubchume@gmail.com>
2021-05-08 16:32:54 +0200

    Install django-heroku

--- photography_anonymizator/settings/production.py
+++ photography_anonymizator/settings/production.py
@@ -1,7 +1,12 @@
 # In a production environment, the next environment variable must be set:
 # DJANGO_SETTINGS_MODULE=photography_anonymizator.settings.production
 
+import django_heroku
+
 from .base import *
 
-
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
